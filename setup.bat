57b8b1ff41493bc5b7c431dd270cd11d123c8129
Vladyslav Krasynskyi <siriusbrightestone@gmail.com>
2024-12-13 00:22:42 +0100

    setup files for windows and linux/macos

---
+++ setup.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Install dependencies
+echo Installing dependencies and Running setup.js
+npm install && node setup.js
+
+pause
---
+++ setup.sh
@@ -0,0 +1,4 @@
+echo "Installing dependencies and Running setup.js..."
+npm install && node setup.js
+
+read -p "Press any key to continue..."
